f8b1bd82239b971bfe23d8b9aaff5a0fc4b39f17
piku235 <piku235@gmail.com>
2015-07-21 22:41:01 +0200

    fixed glyphicons in the bootstrap theme

--- src/Jungi/Bundle/BootstrapThemeBundle/Resources/config/config.rb
+++ src/Jungi/Bundle/BootstrapThemeBundle/Resources/config/config.rb
@@ -2,14 +2,15 @@
 add_import_path "../public/components/bootstrap-sass/assets/stylesheets"
 
 # Set this to the root of your project when deployed:
-http_path = "../public/"
+http_path = "/"
 css_dir = "../public/css"
 sass_dir = "../public/scss"
 images_dir = "../public/images"
 javascripts_dir = "../public/js"
+fonts_dir = "../public/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+# output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
